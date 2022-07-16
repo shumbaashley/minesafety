664d2b47446b72bbc497797339bbc8e3a301bfdd
Shumba Ashley <ashleytshumba@gmail.com>
2022-07-16 23:38:27 +0200

    line chart working-ish

--- readings/urls.py
+++ readings/urls.py
@@ -2,15 +2,16 @@ from django.urls import include, path
 from rest_framework import routers
 from django.views.generic import TemplateView
 from .views import ReadingDataViewSet
+from .views import line_chart, line_chart_json
 
 
 router = routers.DefaultRouter()
-router.register(r'api', ReadingDataViewSet)
+router.register(r"api", ReadingDataViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', TemplateView.as_view(template_name="index.html")),
-    # path('api', ReadingData.as_view(), name='readings' )
+    path("", include(router.urls)),
+    path("", TemplateView.as_view(template_name="index.html")),
+    path("chart", line_chart, name="line_chart"),
+    path("chartJSON", line_chart_json, name="line_chart_json"),
 ]
-
--- readings/views.py
+++ readings/views.py
@@ -4,6 +4,29 @@ from readings.serializers import ReadingDataSerializer
 from rest_framework.response import Response
 
 
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return labels for the x-axis."""
+        return [obj.created for obj in ReadingData.objects.all()]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Temperature", "Humidity", "Gas"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[obj.temperature for obj in ReadingData.objects.all()],[obj.humidity for obj in ReadingData.objects.all()], [obj.gas for obj in ReadingData.objects.all()]]
+
+
+
+line_chart = TemplateView.as_view(template_name='line_chart.html')
+line_chart_json = LineChartJSONView.as_view()
+
 class ReadingDataViewSet(viewsets.ModelViewSet):
     queryset = ReadingData.objects.all().order_by('-created')
     serializer_class = ReadingDataSerializer
@@ -32,3 +55,5 @@ class ReadingDataViewSet(viewsets.ModelViewSet):
             
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
