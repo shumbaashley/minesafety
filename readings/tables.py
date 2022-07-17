ab8c67bfdf2067f45acb46a709de3762c9e12356
Shumba Ashley <ashleytshumba@gmail.com>
2022-07-17 14:54:15 +0200

    bootstrap 4

--- readings/tables.py
+++ readings/tables.py
@@ -4,5 +4,5 @@ from .models import ReadingData
 class ReadingDataTable(tables.Table):
     class Meta:
         model = ReadingData
-        template_name = "django_tables2/bootstrap.html"
+        template_name = "django_tables2/bootstrap4.html"
         fields = ("temperature", "humidity", "gas", "created")
