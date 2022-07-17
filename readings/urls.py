b050d7e8674d907b48edc92574912a1d4f18c11d
Shumba Ashley <ashleytshumba@gmail.com>
2022-07-17 04:10:08 +0200

    good going

--- readings/urls.py
+++ readings/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from django.views.generic import TemplateView
 from .views import GetCurrentReadings, ReadingDataViewSet
 from .views import home_page, line_chart, line_chart_json
+from django.contrib.auth.decorators import login_required
 
 
 router = routers.DefaultRouter()
@@ -12,7 +13,7 @@ router.register(r"api", ReadingDataViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("current-readings/", GetCurrentReadings.as_view(), name="get_current_readings"),
-    path("dashboard/", home_page),
+    path("dashboard/", login_required(home_page)),
     path("chart/", line_chart, name="line_chart"),
     path("chartJSON/", line_chart_json, name="line_chart_json"),
 ]
