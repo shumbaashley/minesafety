4ef818251719c3829ae669d28a41878a09aa4be3
Shumba Ashley <ashleytshumba@gmail.com>
2022-07-19 11:04:13 +0200

    register correctly

--- users/admin.py
+++ users/admin.py
@@ -6,6 +6,10 @@ from .models import User
 
 admin.site.unregister(Group)
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "first_name", "last_name", "date_joined")
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ("username", "first_name", "last_name", "date_joined")
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+admin.site.register(User, UserAdmin)
